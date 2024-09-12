e34473d5d46c4aae606ff1a47887fa849d1da884
vukt2004 <tatcatronghuvo@gmail.com>
2024-09-12 14:26:53 +0700

    Big Update for database

--- Database.sql
+++ Database.sql
@@ -4,15 +4,22 @@ GO
 CREATE DATABASE KoiVeterinaryServiceCenter
 USE KoiVeterinaryServiceCenter
 
+CREATE TABLE Roles
+(
+	roleID VARCHAR(30) PRIMARY KEY NOT NULL,
+	title VARCHAR(100)
+);
+
 CREATE TABLE Users
 (
 	userID VARCHAR(30) PRIMARY KEY NOT NULL,
 	password VARCHAR(20) NOT NULL,
 	name NVARCHAR(100),
 	email VARCHAR(50),
-	phoneNumber VARCHAR(10),
-	role VARCHAR(10) NOT NULL,
-	address NVARCHAR(100)
+	phoneNumber VARCHAR(10) NOT NULL,
+	roleID VARCHAR(30) NOT NULL,
+	address NVARCHAR(100),
+	CONSTRAINT PK_roleID FOREIGN KEY (roleID) REFERENCES Roles(roleID)
 );
 
 CREATE TABLE Veterinarians
@@ -20,6 +27,7 @@ CREATE TABLE Veterinarians
 	veterinaID VARCHAR(30) PRIMARY KEY NOT NULL,
 	rating FLOAT,
 	userID VARCHAR(30) NOT NULL,
+	status VARCHAR(10) NOT NULL,
 	CONSTRAINT PK_userID FOREIGN KEY (userID) REFERENCES Users(userID)
 );
 
@@ -28,7 +36,30 @@ CREATE TABLE Services
 	serviceID VARCHAR(30) PRIMARY KEY NOT NULL,
 	name NVARCHAR(50) NOT NULL,
 	type VARCHAR(50),
-	price INT,
+	price FLOAT,
+);
+
+CREATE TABLE TravelExpenses
+(
+	ExpenseID VARCHAR(30) PRIMARY KEY NOT NULL,
+	ExpenseAmount FLOAT NOT NULL,
+	startLocation VARCHAR(50) NOT NULL, 
+	endLocation VARCHAR(50) NOT NULL,
+	--CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Orders
+(
+	orderID VARCHAR(30) PRIMARY KEY NOT NULL,
+	serviceID VARCHAR(30) NOT NULL,
+	veterinaID VARCHAR(30) NOT NULL,
+	userID VARCHAR(30) NOT NULL,
+	orderDate DATE,
+	startTime TIME,
+	endTime TIME,
+	CONSTRAINT PK_userID_2 FOREIGN KEY (userID) REFERENCES Users(userID),
+	CONSTRAINT PK_serviceID FOREIGN KEY (serviceID) REFERENCES Services(serviceID),
+	CONSTRAINT PK_veterinaID FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
 );
 
 CREATE TABLE Invoice
@@ -38,18 +69,28 @@ CREATE TABLE Invoice
 	veterinaID VARCHAR(30) NOT NULL,
 	total INT,
 	invDate DATE,
-	CONSTRAINT PK_userID_2 FOREIGN KEY (userID) REFERENCES Users(userID),
-	CONSTRAINT PK_veterinaID FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
+	CONSTRAINT PK_userID_3 FOREIGN KEY (userID) REFERENCES Users(userID),
+	CONSTRAINT PK_veterinaID_2 FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
+);
+
+CREATE TABLE InvoiceDetails
+(
+	invoiceID VARCHAR(30) NOT NULL,
+	orderID VARCHAR(30) NOT NULL,
+	PRIMARY KEY(orderID, invoiceID),
+	quantity INT,
+	CONSTRAINT PK_invoiceID_2 FOREIGN KEY (invoiceID) REFERENCES Invoice(invoiceID),
+	CONSTRAINT PK_orderID_2 FOREIGN KEY (orderID) REFERENCES Orders(orderID)
 );
 
 CREATE TABLE Schedule
 (
+	scheduleID VARCHAR(30) NOT NULL PRIMARY KEY,
 	veterinaID VARCHAR(30) NOT NULL,
 	scheduleDate DATE NOT NULL,
-	period TIME NOT NULL,
-	status BIT NOT NULL,
-	PRIMARY KEY(scheduleDate, period),
-	CONSTRAINT PK_veterinaID_2 FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
+	startTime TIME NOT NULL,
+	endTime TIME NOT NULL,
+	CONSTRAINT PK_veterinaID_3 FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
 );
 
 CREATE TABLE Feedback 
@@ -59,6 +100,7 @@ CREATE TABLE Feedback
 	PRIMARY KEY(feedbackID, invoiceID),
 	comment VARCHAR(100),
 	rating FLOAT,
+	feedbackDateTime DATETIME,
 	CONSTRAINT PK_invoiceID FOREIGN KEY (invoiceID) REFERENCES INVOICE(invoiceID)
 );
 
@@ -74,13 +116,11 @@ CREATE TABLE FishInformation
 	CONSTRAINT PK_userID_4 FOREIGN KEY (userID) REFERENCES Users(userID)
 );
 
-CREATE TABLE InvoiceDetails
+CREATE TABLE Proccess
 (
-	invoiceID VARCHAR(30) NOT NULL,
-	serviceID VARCHAR(30) NOT NULL,
-	PRIMARY KEY(serviceID, invoiceID),
-	quantity INT,
-	CONSTRAINT PK_invoiceID_2 FOREIGN KEY (invoiceID) REFERENCES Invoice(invoiceID),
-	CONSTRAINT PK_serviceID FOREIGN KEY (serviceID) REFERENCES Services(serviceID)
+	proccessID VARCHAR(30) PRIMARY KEY NOT NULL,
+	veterinaID VARCHAR(30) NOT NULL,
+	Description VARCHAR(100),
+	status BIT NOT NULL,
+	CONSTRAINT PK_veterinaID_4 FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
 );
-
