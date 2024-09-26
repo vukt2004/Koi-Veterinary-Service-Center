2dd3b0a9b9da211cdd9eb6f10a44234a49811157
light0504 <lightlan0504@gmail.com>
2024-09-26 13:28:15 +0700

    Final database

--- Database.sql
+++ Database.sql
@@ -1,31 +1,50 @@
--- Create table for Roles
+-- Table: Roles
 CREATE TABLE Roles (
-    roleID VARCHAR(30) PRIMARY KEY NOT NULL,
-    title VARCHAR(100)
+    roleID CHAR(1) PRIMARY KEY NOT NULL,
+    title VARCHAR(10)
 );
 
--- Create table for Users
+-- Table: Slots
+CREATE TABLE Slots (
+    slot INT PRIMARY KEY NOT NULL,
+    startTime TIME,
+    endTime TIME
+);
+
+-- Table: Users
 CREATE TABLE Users (
     userID VARCHAR(30) PRIMARY KEY NOT NULL,
     password VARCHAR(20) NOT NULL,
     name NVARCHAR(100),
     email VARCHAR(50),
     phoneNumber VARCHAR(10) NOT NULL,
-    roleID VARCHAR(30) NOT NULL,
+    roleID CHAR(1) NOT NULL,
     address NVARCHAR(100),
     FOREIGN KEY (roleID) REFERENCES Roles(roleID)
 );
 
--- Create table for Veterinarians
-CREATE TABLE Veterinarians (
+-- Table: Veterina
+CREATE TABLE Veterina (
     veterinaID VARCHAR(30) PRIMARY KEY NOT NULL,
-    rating FLOAT,
     userID VARCHAR(30) NOT NULL,
-    status VARCHAR(10) NOT NULL,
+    description NVARCHAR(200),
+    status BIT,
     FOREIGN KEY (userID) REFERENCES Users(userID)
 );
 
--- Create table for Services
+-- Table: Fish
+CREATE TABLE Fish (
+    userID VARCHAR(30) NOT NULL,
+    fishID VARCHAR(30) NOT NULL,
+    weight FLOAT,
+    length FLOAT,
+    months INT,
+    description NVARCHAR(100),
+    PRIMARY KEY (userID, fishID),
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+-- Table: Services
 CREATE TABLE Services (
     serviceID VARCHAR(30) PRIMARY KEY NOT NULL,
     name NVARCHAR(50) NOT NULL,
@@ -33,14 +52,14 @@ CREATE TABLE Services (
     price FLOAT
 );
 
--- Create table for TravelExpenses
+-- Table: TravelExpenses
 CREATE TABLE TravelExpenses (
     ExpenseID VARCHAR(30) PRIMARY KEY NOT NULL,
     Fee INT NOT NULL,
     endLocation VARCHAR(50) NOT NULL
 );
 
--- Create table for Orders
+-- Table: Orders
 CREATE TABLE Orders (
     orderID VARCHAR(30) PRIMARY KEY NOT NULL,
     veterinaID VARCHAR(30) NOT NULL,
@@ -49,14 +68,15 @@ CREATE TABLE Orders (
     slot INT,
     ExpenseID VARCHAR(30) NOT NULL,
     address VARCHAR(100) NOT NULL,
-    Description VARCHAR(255),
+    description NVARCHAR(255),
     status VARCHAR(10),
-    FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID),
+    FOREIGN KEY (veterinaID) REFERENCES Veterina(veterinaID),
     FOREIGN KEY (userID) REFERENCES Users(userID),
+    FOREIGN KEY (slot) REFERENCES Slots(slot),
     FOREIGN KEY (ExpenseID) REFERENCES TravelExpenses(ExpenseID)
 );
 
--- Create table for Invoice
+-- Table: Invoice
 CREATE TABLE Invoice (
     invoiceID VARCHAR(30) PRIMARY KEY NOT NULL,
     orderID VARCHAR(30) NOT NULL,
@@ -65,7 +85,7 @@ CREATE TABLE Invoice (
     FOREIGN KEY (orderID) REFERENCES Orders(orderID)
 );
 
--- Create table for OrderDetails
+-- Table: OrderDetails
 CREATE TABLE OrderDetails (
     orderID VARCHAR(30) NOT NULL,
     serviceID VARCHAR(30) NOT NULL,
@@ -75,20 +95,11 @@ CREATE TABLE OrderDetails (
     FOREIGN KEY (serviceID) REFERENCES Services(serviceID)
 );
 
--- Create table for Schedule
-CREATE TABLE Schedule (
-    scheduleID VARCHAR(30) PRIMARY KEY NOT NULL,
-    veterinaID VARCHAR(30) NOT NULL,
-    scheduleDate DATE NOT NULL,
-    slot INT,
-    FOREIGN KEY (veterinaID) REFERENCES Veterinarians(veterinaID)
-);
-
--- Create table for Feedback
+-- Table: Feedback
 CREATE TABLE Feedback (
     feedbackID VARCHAR(30) PRIMARY KEY NOT NULL,
     invoiceID VARCHAR(30) NOT NULL,
-    comment VARCHAR(100),
+    comment NVARCHAR(100),
     rating FLOAT,
     feedbackDateTime DATETIME,
     FOREIGN KEY (invoiceID) REFERENCES Invoice(invoiceID)
