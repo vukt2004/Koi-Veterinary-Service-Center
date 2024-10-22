539c73d49e42b7024223eafafbf069051f111407
nguyenngoctuananh3001 <nguyenngoctuananh3001@gmail.com>
2024-10-22 15:52:54 +0700

    Update Report

---
+++ Koi-Veterinary-Service-Center-API/src/main/java/com/fpt/Koi_Veterinary_Service_Center_API/service/IReportService.java
@@ -0,0 +1,7 @@
+package com.fpt.Koi_Veterinary_Service_Center_API.service;
+
+import com.fpt.Koi_Veterinary_Service_Center_API.dto.response.reportResponse;
+
+public interface IReportService {
+    reportResponse getAllReport();
+}
