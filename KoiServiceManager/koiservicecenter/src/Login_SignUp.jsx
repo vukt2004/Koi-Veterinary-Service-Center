c0255d874172358f0e4f61835c394613d29c9f37
light0504 <lightlan0504@gmail.com>
2024-11-06 16:31:49 +0700

    Fix sth

--- KoiServiceManager/koiservicecenter/src/Login_SignUp.jsx
+++ KoiServiceManager/koiservicecenter/src/Login_SignUp.jsx
@@ -1,59 +1,72 @@
-import { useState, useEffect } from 'react';
-import api from '../src/config/axios'
-import './Login_SignUp.css';  // Assuming the styles are similar, CSS file is reused.
+﻿import { useState, useEffect } from 'react';
+import api from '../src/config/axios';
+import './Login_SignUp.css';
 import { useNavigate } from 'react-router-dom';
+
+const userLocation = [
+    "Online",
+    "Quận 1",
+    "Quận 2",
+    "Quận 3",
+    "Quận 4",
+    "Quận 5",
+    "Quận 6",
+    "Quận 7",
+    "Quận 8",
+    "Quận 9",
+    "Quận 10",
+    "Quận 11",
+    "Quận 12",
+    "Quận Bình Tân",
+    "Quận Bình Thạnh",
+    "Quận Gò Vấp",
+    "Quận Phú Nhuận",
+    "Quận Tân Bình",
+    "Quận Tân Phú",
+    "Huyện Bình Chánh",
+    "Huyện Củ Chi",
+    "Huyện Cần Giờ",
+    "Huyện Hóc Môn",
+    "Huyện Nhà Bè"
+];
 
 function LoginSignUp() {
     const [userID, setUserID] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [fullName, setFullName] = useState('');
     const [confirmPass, setConfirmPass] = useState('');
+    const [fullName, setFullName] = useState('');
     const [phone, setPhone] = useState('');
+    const [selectedAddress, setSelectedAddress] = useState('');
+    const [addressDetails, setAddressDetails] = useState('');
     const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState('');
     const navigate = useNavigate();
-
 
     useEffect(() => {
         sessionStorage.removeItem('user');
         sessionStorage.removeItem('role');
     }, []);
+
     const handleSubmitLogin = async (e) => {
         e.preventDefault();
         setLoading(true);
-        setMessage(null);
         try {
             const response = await api.post('/login', {
                 userID,
                 password,
             });
-
-            const data = response.data; // Assuming the token is returned in the response
-            sessionStorage.setItem('user', data.token); // Save token in local storage
-            sessionStorage.setItem('role', data.role);
-            console.log(sessionStorage.getItem('user'));
-            console.log(sessionStorage.getItem('role'));
-            
-            if (data.role === 'C') {
-                navigate('/');
-            } else if (data.role === 'V') {
-                navigate('/veterinaPage');
-            } else if (data.role === 'S') {
-                navigate('/staffPage');
-            } else if (data.role === 'M') {
-                navigate('/managerPage')
+            if (response) {
+                const data = response.data;
+                sessionStorage.setItem('user', data.token);
+                sessionStorage.setItem('role', data.role);
+                navigate(data.role === 'C' ? '/' : data.role === 'V' ? '/veterinaPage' : data.role === 'S' ? '/staffPage' : '/managerPage');
+                window.location.reload();
+            } else {
+                window.alert("Tài khoản hoặc mật khẩu không đúng");
             }
-            window.location.reload();
-            
-            
-
-            // Handle successful login (e.g., save token, redirect, etc.)
         } catch (error) {
-            console.log(error.response ? error.response.data.message : 'Sign In failed!');
-            setMessage('Fail to log in!')
+            window.alert("Lôi khi login");
         } finally {
-            setUserID('')
-            setPassword('')
             setLoading(false);
         }
     };
@@ -61,100 +74,117 @@ function LoginSignUp() {
     const handleSubmitRegister = async (e) => {
         e.preventDefault();
         setLoading(true);
-        setMessage(null);
         if (password !== confirmPass) {
-            setMessage('Passwords do not match!');
+            window.alert('Nhập lại mật khẩu không khớp!');
+            setLoading(false);
             return;
         }
+        const completeAddress = selectedAddress === 'Online' ? 'Online' : `${addressDetails}, ${selectedAddress}`;
         try {
-            const user = { ID: { userID }, name: { fullName }, pass: { password }, phoneNumber: { phone } }
-            localStorage.setItem('userSignUp', JSON.stringify(user));
-
+            const user = { userID: userID, email, password, name: fullName, phoneNumber: phone, address: completeAddress };
+            const response = await api.post('/register', user);
+            if (response) {
+                window.alert('Đăng kí thành công');
+            } else {
+                window.alert('Đăng kí thất bại');
+            }
+            navigate('/login');
         } catch (error) {
-            setMessage(error.response ? error.response.data.message : 'Sign Up failed!');
+            window.alert(error.response ? error.response.data : 'Đăng kí thất bại!');
         } finally {
             setLoading(false);
         }
-
-    }
-
-    // Hook to manage form state - signup or signin
-    const [isSignUp, setIsSignUp] = useState(false);
-
-    // Toggle the signup/signin state
-    const toggleSignUp = () => {
-        setIsSignUp(!isSignUp);
     };
 
+    const [isSignUp, setIsSignUp] = useState(false);
+    const toggleSignUp = () => setIsSignUp(!isSignUp);
+
     return (
-        <>
-            <div className={`cont ${isSignUp ? 's--signup' : ''}`}>
-                {/* Sign In Form */}
-                <div className="form sign-in">
-                    <h2>Welcome</h2>
-                    <label>
-                        <span>User ID</span>
-                        <input type="text" value={userID}
-                            onChange={(e) => setUserID(e.target.value)} required />
-                    </label>
-                    <label>
-                        <span>Password</span>
-                        <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </label>
-                    <button type="button" className="submit" onClick={handleSubmitLogin} disabled={loading}>{loading ? 'Processing...' : 'Sign In'}</button>
+        <div className={`cont ${isSignUp ? 's--signup' : ''}`}>
+            <div className="form sign-in">
+                <h2>Xin chào</h2>
+                <label>
+                    <span>Tài khoản</span>
+                    <input type="text" value={userID} onChange={(e) => setUserID(e.target.value)} required />
+                </label>
+                <label>
+                    <span>Mật khẩu</span>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </label>
+                <button type="button" className="submit" onClick={handleSubmitLogin} disabled={loading}>
+                    {loading ? 'Vui lòng đợi' : 'Đăng nhập'}
+                </button>
+            </div>
+
+            <div className="sub-cont">
+                <div className="img">
+                    <div className={`img__text ${isSignUp ? '' : 'm--in'}`}>
+                        <h3>Nếu đã có tài khoản, đăng nhập ngay.</h3>
+                    </div>
+                    <div className={`img__text ${isSignUp ? 'm--up' : ''}`}>
+                        <h3>Không có tài khoản? Đăng kí!</h3>
+                    </div>
+                    <div className="img__btn" onClick={toggleSignUp}>
+                        <span className="m--up">Đăng kí</span>
+                        <span className="m--in">Đăng nhập</span>
+                    </div>
                 </div>
 
-                {/* Image and Toggle Text */}
-                <div className="sub-cont">
-                    <div className="img">
-                        <div className={`img__text ${isSignUp ? '' : 'm--in'}`}>
-                            <h3>{"If you already have an account, just sign in."}</h3>
-                        </div>
-                        <div className={`img__text ${isSignUp ? 'm--up' : ''}`}>
-                            <h3>{"Don't have an account? Please Sign up!"}</h3>
-                        </div>
-                        <div className="img__btn" onClick={toggleSignUp}>
-                            <span className="m--up">Sign Up</span>
-                            <span className="m--in">Sign In</span>
-                        </div>
-                    </div>
-
-                    {/* Sign Up Form */}
-                    <div className="form sign-up">
-                        <h2>Create your Account</h2>
+                <div className="form sign-up">
+                    <h2>Tạo tài khoản mới</h2>
+                    <label>
+                        <span>Tài khoản</span>
+                        <input type="text" value={userID} onChange={(e) => setUserID(e.target.value)} required />
+                    </label>
+                    <label>
+                        <span>Mật khẩu</span>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    </label>
+                    <label>
+                        <span>Nhập lại mật khẩu</span>
+                        <input type="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} required />
+                    </label>
+                    <label>
+                        <span>Họ và tên</span>
+                        <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} required />
+                    </label>
+                    <label>
+                        <span>Email</span>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    </label>
+                    <label>
+                        <span>Số điện thoại</span>
+                        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+                    </label>
+                    <label>
+                        <span>Địa chỉ của bạn</span><br></br>
+                        <select value={selectedAddress} onChange={(e) => setSelectedAddress(e.target.value)} required>
+                            <option value="">-- Chọn quận/huyện (Online nếu ngoài Tp.HCM) --</option>
+                            {userLocation.map((location) => (
+                                <option key={location} value={location}>
+                                    {location}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                    {selectedAddress && selectedAddress !== 'Online' && (
                         <label>
-                            <span>User ID</span>
-                            <input type="text" value={userID}
-                                onChange={(e) => setUserID(e.target.value)} required />
+                            <span>Địa chỉ chi tiết</span>
+                            <input
+                                type="text"
+                                value={addressDetails}
+                                onChange={(e) => setAddressDetails(e.target.value)}
+                                placeholder="Enter additional address details"
+                                required
+                            />
                         </label>
-                        <label>
-                            <span>Full Name</span>
-                            <input type="text" value={fullName}
-                                onChange={(e) => setFullName(e.target.value)} required />
-                        </label>
-                        <label>
-                            <span>Password</span>
-                            <input type="password" value={password}
-                                onChange={(e) => setPassword(e.target.value)} required />
-                        </label>
-                        <label>
-                            <span>Confirm Password</span>
-                            <input type="password" value={confirmPass}
-                                onChange={(e) => setConfirmPass(e.target.value)} required />
-
-                        </label>
-                        <label>
-                            <span>Phone Number</span>
-                            <input type="text" value={phone}
-                                onChange={(e) => setPhone(e.target.value)} required />
-                        </label>
-                        <button type="button" className="submit" onClick={handleSubmitRegister} disabled={loading}>{loading ? 'Processing...' : 'Sign Up'}</button>
-                    </div>
+                    )}
+                    <button type="button" className="submit" onClick={handleSubmitRegister} disabled={loading}>
+                        {loading ? 'Vui lòng đợi...' : 'Đăng kí'}
+                    </button>
                 </div>
             </div>
-            {message && <h3>{message}</h3>}
-        </>
-
+        </div>
     );
 }
 
