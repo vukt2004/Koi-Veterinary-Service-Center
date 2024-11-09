4e732ebc630b52e0f0d15857448f446b6ee9b076
light0504 <lightlan0504@gmail.com>
2024-11-09 15:23:22 +0700

    Change service

--- KoiServiceManager/koiservicecenter/src/component/Service.jsx
+++ KoiServiceManager/koiservicecenter/src/component/Service.jsx
@@ -20,31 +20,33 @@ const CustomerServices = () => {
     return (
         <div className="service-container">
             <h1 className="service-title">Dịch Vụ Cá Koi</h1>
-
-            <table className="service-table">
-                <thead>
-                    <tr>
-                        <th>Tên Dịch Vụ</th>
-                        <th>Loại</th>
-                        <th>Giá (VND)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dichVu.length > 0 ? (
-                        dichVu.sort((dv)=>(dv.name)).filter(dv => dv.type !== 'Thuốc').map((dv) => (
+            {dichVu.length > 0 ? (
+                <>
+                    <table className="service-table">
+                        <thead>
+                            <tr>
+                                <th>Tên Dịch Vụ</th>
+                                <th>Loại</th>
+                                <th>Giá (VND)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dichVu.map((dv) => (
                                 <tr key={dv.serviceID}>
                                     <td>{dv.name}</td>
-                                    <td>{dv.type}</td>
+                                    <td>{dv.type} {dv.service ? '' : '*'}</td>
                                     <td>{dv.price.toLocaleString('vi-VN')} VND</td>
                                 </tr>
-                            ))
-                    ) : (
-                        <tr>
-                            <td colSpan="3" className="service-no-data">Không có dịch vụ nào.</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+                            ))}
+                        </tbody>
+                    </table>
+                    <br></br>
+                    <h3>Dịch vụ đánh dấu * cần bác sĩ chỉ định</h3>
+                </>
+                
+            ) : (
+                <h2>Hiện tại không có dịch vụ nào.</h2>
+            )}
         </div>
     );
 };
